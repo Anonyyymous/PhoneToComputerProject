0ee45501e58829be6b08bfa4eb0a918d99720483
Anonyyymous <78022284+Anonyyymous@users.noreply.github.com>
2024-02-04 09:00:22 +0000

    removed some comments

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-# from Encryption import *
 from Matchmaking import *
-# from MouseControl import *
 from KivyClasses import MyApp
-# import customtkinter
-# import kivy
 
-# data = input()
-# data = "1.53.254.74"  # "jjjk...hfahf123',"
-# ip = get_ip()
-# print(space_string(encrypt(ip)))
 
 app = MyApp()
 app.run()
 app.on_close()
-
-# make it so that we kill the server thread on kivy.close()
-
-inp = ""
-'''while inp == "":
-    inp = input("")
-    vector = Vector(int(input("enter x")), int(input("enter y")))
-    mouse.move_mouse(vector)'''
