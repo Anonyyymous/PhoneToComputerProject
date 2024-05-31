ff73eb3e624a5dd57a4b14a7263cd5428e17b77f
Anonyyymous <78022284+Anonyyymous@users.noreply.github.com>
2024-05-31 18:28:52 +0100

    Added .gitignore

--- main2.py
+++ main2.py
@@ -1,8 +1,10 @@
 from Matchmaking import *
 from KivyClasses import MyApp
 
+# didnt run from main.py, i have genuinely no idea why i think im being haunted
 
 print(__name__)
+
 app = MyApp()
 app.run()
 app.on_close()
